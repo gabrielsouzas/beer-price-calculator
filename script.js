41e253e8f72252ea3724c75b1f9663e7db42f236
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2023-05-02 15:49:54 -0300

    Calculation logic

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const fifty = document.querySelector('#fifty')
+const twoHundredSixtyNine = document.querySelector('#two_hundred_sixty_nine');
 const threeHundred = document.querySelector('#three_hundred')
 const threeHundredFifty = document.querySelector('#three_hundred_fifty')
 const thousand = document.querySelector('#thousand')
@@ -9,9 +10,21 @@ const selectMl = document.querySelector('#sel_ml')
 function Calculate({target}) {
     const price = target.value;
     if (price) {
-        const fiftyMlCost = (Number(price)/(Number(selectMl.value)/50)).toFixed(2);
-        fifty.innerHTML = fiftyMlCost;
+        const fiftyMlCost = (Number(price)/(Number(selectMl.value)/50));
+        fifty.innerHTML = fiftyMlCost.toFixed(3);
 
-        threeHundred.innerHTML = (fiftyMlCost*(Number(selectMl.value)/50))
+        twoHundredSixtyNine.innerHTML = CalculateValueMl(fiftyMlCost, 269);
+
+        threeHundred.innerHTML = CalculateValueMl(fiftyMlCost, 300);
+
+        threeHundredFifty.innerHTML = CalculateValueMl(fiftyMlCost, 350);
+
+        thousand.innerHTML = CalculateValueMl(fiftyMlCost, 1000);
+
+        fourThousandTwoHundred.innerHTML = CalculateValueMl(fiftyMlCost, 4200);
     }
+}
+
+function CalculateValueMl(fifty_ml_cost, selected_ml) {
+    return (fifty_ml_cost*(Number(selected_ml)/50)).toFixed(2);
 }
