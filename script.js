4180a32436fceb6e071917586981bc4517da2465
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2023-05-02 17:06:12 -0300

    Loop more fields

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
-const priceInput = document.querySelector('#price_input')
+const priceInput = document.querySelectorAll('.price_input')
 
-const fifty = document.querySelector('#fifty')
-const twoHundredSixtyNine = document.querySelector('#two_hundred_sixty_nine');
-const threeHundred = document.querySelector('#three_hundred')
-const threeHundredFifty = document.querySelector('#three_hundred_fifty')
-const thousand = document.querySelector('#thousand')
-const fourThousandTwoHundred = document.querySelector('#four_thousand_two_hundred')
+const fifty = document.querySelectorAll('.fifty')
+const twoHundredSixtyNine = document.querySelectorAll('.two_hundred_sixty_nine');
+const threeHundred = document.querySelectorAll('.three_hundred')
+const threeHundredFifty = document.querySelectorAll('.three_hundred_fifty')
+const thousand = document.querySelectorAll('.thousand')
+const fourThousandTwoHundred = document.querySelectorAll('.four_thousand_two_hundred')
 
-const selectMl = document.querySelector('#sel_ml')
+const selectMl = document.querySelectorAll('.sel_ml')
 
 function Calculate(event) {
-    var price = 0;
-    if (event.target === undefined) {
-        price = event.value;
-    } else {
-        price = event.target.value;
-    }
+    if (event) {
+        var price = 0;
+        var index = 0;
+        if (event.target === undefined) {
+            price = event.value;
+            index = Number(event.dataset.index)
+        } else {
+            price = event.target.value;
+            index = Number(event.target.dataset.index);
+        }
     
-    if (price) {
-        const fiftyMlCost = (Number(price)/(Number(selectMl.value)/50));
-        fifty.innerHTML = fiftyMlCost.toFixed(3);
+        const fiftyMlCost = (Number(price)/(Number(selectMl[index].value)/50));
+        fifty[index].innerHTML = fiftyMlCost.toFixed(3);
 
-        twoHundredSixtyNine.innerHTML = CalculateValueMl(fiftyMlCost, 269);
-
-        threeHundred.innerHTML = CalculateValueMl(fiftyMlCost, 300);
-
-        threeHundredFifty.innerHTML = CalculateValueMl(fiftyMlCost, 350);
-
-        thousand.innerHTML = CalculateValueMl(fiftyMlCost, 1000);
-
-        fourThousandTwoHundred.innerHTML = CalculateValueMl(fiftyMlCost, 4200);
+        twoHundredSixtyNine[index].innerHTML = CalculateValueMl(fiftyMlCost, 269);
+        threeHundred[index].innerHTML = CalculateValueMl(fiftyMlCost, 300);
+        threeHundredFifty[index].innerHTML = CalculateValueMl(fiftyMlCost, 350);
+        thousand[index].innerHTML = CalculateValueMl(fiftyMlCost, 1000);
+        fourThousandTwoHundred[index].innerHTML = CalculateValueMl(fiftyMlCost, 4200);
     }
 }
 
@@ -37,7 +36,8 @@ function CalculateValueMl(fifty_ml_cost, selected_ml) {
     return (fifty_ml_cost*(Number(selected_ml)/50)).toFixed(2);
 }
 
-selectMl.addEventListener('change', () => {
-    //console.log(priceInput.innerHTML)
-    Calculate(priceInput)
-})
+selectMl.forEach(element => {
+    element.addEventListener('change', () => {
+        Calculate(priceInput[Number(element.dataset.index)]);
+    })
+});
