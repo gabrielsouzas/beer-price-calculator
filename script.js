01610ce854688795da651cc1aa5904ac6a590bb9
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2023-05-02 17:29:33 -0300

    Create Element

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const fourThousandTwoHundred = document.querySelectorAll('.four_thousand_two_hun
 
 const selectMl = document.querySelectorAll('.sel_ml')
 
+var lastIndex = 0;
+
 function Calculate(event) {
     if (event) {
         var price = 0;
@@ -41,3 +43,71 @@ selectMl.forEach(element => {
         Calculate(priceInput[Number(element.dataset.index)]);
     })
 });
+
+function addLineBeer(){
+    const div = createElement('div');
+    const selectBrand = createSelectBrand();
+    
+    const selectMl = createSelectMl();
+    
+    const input = createElement('input');
+    const labelA = createElement('label');
+    const labelB = createElement('label');
+    const labelC = createElement('label');
+    const labelD = createElement('label');
+    const labelE = createElement('label');
+    const labelF = createElement('label');
+
+
+    /*<div>
+                <select>
+                    <option value="Antartica">Antartica</option>
+                </select>
+                <select data-index="1" class="sel_ml">
+                    <option value="269">269</option>
+                    <option value="300" selected>300</option>
+                    <option value="350">350</option>
+                    <option value="1000">1000</option>
+                    <option value="4200">4200</option>
+                </select>
+                <input data-index="1" class="price_input" type="text" oninput="Calculate(event)">
+                <label data-index="1" class="fifty">0,00</label>
+                <label data-index="1" class="two_hundred_sixty_nine">0,00</label>
+                <label data-index="1" class="three_hundred">0,00</label>
+                <label data-index="1" class="three_hundred_fifty">0,00</label>
+                <label data-index="1" class="thousand">0,00</label>
+                <label data-index="1" class="four_thousand_two_hundred">0,00</label>
+            </div>*/
+}
+
+const createSelectMl = () => {
+    const options = `
+        <option value="269">269</option>
+        <option value="300" selected>300</option>
+        <option value="350">350</option>
+        <option value="1000">1000</option>
+        <option value="4200">4200</option>
+    `;
+
+    const select = createElement('select', '', options);
+    
+    return select;
+}
+
+const createSelectBrand = () => {
+    const options = `
+        <option value="Antartica">Antartica</option>
+        <option value="Skol">Skol</option>
+        <option value="Brahma">Brahma</option>
+        <option value="Sub-Zero">Sub-Zero</option>
+        <option value="Budweiser">Budweiser</option>
+        <option value="Stella Artois">Stella Artois</option>
+        <option value="Burguesa">Burguesa</option>
+        <option value="Conti">Conti</option>
+        <option value="Amstel">Amstel</option>
+    `;
+
+    const select = createElement('select', '', options);
+    
+    return select;
+}
